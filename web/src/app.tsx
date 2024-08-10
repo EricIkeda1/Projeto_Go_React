7e8df1c7869b0472b514e8c3268f18d6f2d20f56
ikedayuji <ikedayuji.2002@gmail.com>
2024-08-10 11:06:08 -0300

    Aula 04 finalizado

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,26 +2,26 @@ import { Toaster } from 'sonner'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { CreateRoom } from './pages/create-room'
 import { Room } from './pages/room'
- 
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from './lib/react-query'
+
 const router = createBrowserRouter([
   {
     path: '/',
     element: <CreateRoom />
   },
   {
-    path: '/room/:roomID',
-    element: <Room/>
+    path: '/room/:roomId',
+    element: <Room />
   }
 ])
 
 export function App() {
-
-  return(
-    <>
-  <RouterProvider router={router} />
-    <Toaster invert richColors />
-    </>
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <Toaster invert richColors />
+    </QueryClientProvider>
   )
 }
-
 
